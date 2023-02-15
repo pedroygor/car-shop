83e5a1351fca0549c499018636aeefb28f4103ac
pedroygor <pedroygor2010@hotmail.com>
2023-02-15 15:05:09 -0300

    test: in MotorcycleService -> add tests for update and delete

--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -5,6 +5,10 @@ import MotorcycleService from '../../../src/Services/MotorcycleService';
 import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
 
 describe('MotorcycleService', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
   describe('create', function () {
     it('should return a new motorcycle', async function () {
       const motorcycleInput: IMotorcycle = {
@@ -122,7 +126,57 @@ describe('MotorcycleService', function () {
     });
   });
 
-  afterEach(function () {
-    sinon.restore();
+  describe('update', function () {
+    it('should return a motorcycle updated', async function () {
+      const motorcycleInput: IMotorcycle = {
+        model: 'model',
+        year: 2020,
+        color: 'red',
+        status: true,
+        buyValue: 1000,
+        category: 'Street',
+        engineCapacity: 1000,
+      };
+
+      const motorcycleOutput: IMotorcycle = {
+        id: '633ec9fa3df977e30e993492',
+        model: 'model',
+        year: 2020,
+        color: 'red',
+        status: true,
+        buyValue: 1000,
+        category: 'Street',
+        engineCapacity: 1000,
+      };
+
+      sinon.stub(Model, 'findByIdAndUpdate').resolves(motorcycleOutput);
+
+      const service = new MotorcycleService();
+      const result = await service.update('633ec9fa3df977e30e993492', motorcycleInput);
+
+      expect(result).to.be.deep.equal(motorcycleOutput);
+    });
+  });
+
+  describe('delete', function () {
+    it('should return a motorcycle deleted', async function () {
+      const motorcycleOutput: IMotorcycle = {
+        id: '633ec9fa3df977e30e993492',
+        model: 'model',
+        year: 2020,
+        color: 'color',
+        status: true,
+        buyValue: 1000,
+        category: 'Street',
+        engineCapacity: 1000,
+      };
+
+      sinon.stub(Model, 'findByIdAndDelete').resolves(motorcycleOutput);
+
+      const service = new MotorcycleService();
+      const result = await service.delete('633ec9fa3df977e30e993492');
+
+      expect(result).to.be.deep.equal(motorcycleOutput);
+    });
   });
 });
